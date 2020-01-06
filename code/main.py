9c4c507801aff4d762d2b0580b8d72aa4ddc8975
Minh Quang Trần <quang.tran@eastagile.com>
2020-01-06 03:29:25 +0000

    first success run

--- code/main.py
+++ code/main.py
@@ -1,27 +1,97 @@
-COVERAGE_INIT = "\nm.global.testCoverage[\"{}\"] = CreateObject(\"roArray\", {}, false)"
-COVERAGE_LINE = "m.global.testCoverage[\"{}\"][{}] = true"
+# COVERAGE_INIT = "\nm.global.testCoverage[\"{}\"] = CreateObject(\"roArray\", {}, false)"
+COVERAGE_INIT = r"""
+testCoverage = m.global.testCoverage
+testCoverage["{}"] = CreateObject("roArray", {}, false)
+m.global.testCoverage = testCoverage"""
 
-MAIN_COVERAGE_LINES = r"""m.global.addFields({
-    testCoverage: {}
-    testCoverageReport: {}
+# MARK_TEST_FUNC = """sub {0}_markTestCoverage(index)
+#   componentCoverage = m.global.testCoverage["{0}"]
+#   if componentCoverage = invalid
+#     testCoverage = m.global.testCoverage
+#     testCoverage["{0}"] = createObject("roSGNode", "ContentNode")
+#     testCoverage["{0}"].addFields({{"length":{1}}})
+#     m.global.testCoverage = testCoverage
+#     componentCoverage = m.global.testCoverage["{0}"]
+#   end if
+#   index = str(index)
+#   m.global.testCoverage["{0}"].addFields({{index:true}})
+# end sub
+
+# """
+# MARK_TEST_FUNC = """sub {0}_markTestCoverage(index)
+#   'if m.{0}Coverage = invalid
+#     'm.{0}Coverage = m.global.testCoverage.findNode("{0}")
+#   'end if
+#   if m.{0}Coverage = invalid
+#     m.{0}Coverage = m.global.testCoverage.createChild("ContentNode")
+#     m.{0}Coverage.id = "{0}"
+#     m.{0}Coverage.addFields({{"length":{1}}})
+#   end if
+#   index = str(index)
+#   fields = {{}}
+#   fields[index] = true
+#   m.{0}Coverage.addFields(fields)
+# end sub
+
+# """
+MARK_TEST_FUNC = """sub {0}_markTestCoverage(index)
+  'if m.global.testCoverage.{0} = invalid
+  m.global.testCoverage.addFields({{{0}: createobject("roSGNode","ContentNode")}})
+  m.global.testCoverage.{0}.addFields({{"length":{1}}})
+  'end if
+  index = index.toStr()
+  fields = {{}}
+  fields[index] = true
+  m.global.testCoverage.{0}.addFields(fields)
+end sub
+
+"""
+
+# COVERAGE_LINE = "m.global.testCoverage[\"{}\"][{}] = true"
+COVERAGE_LINE = "{}_markTestCoverage({})"
+
+MAIN_COVERAGE_LINES = r"""  m.global.addFields({
+    testCoverage: createObject("roSGNode", "ContentNode")
   })"""
 
-REPORT_COVERAGE_LINES = r"""    for component in m.global.testCoverage
+REPORT_COVERAGE_LINES = r"""    totalLinesCount = 0
+    totalUncoveredCount = 0
+    for each componentName in m.global.testCoverage.getFields()
+      if componentName <> "change" and componentName <> "focusable" and componentName <> "focusedChild" and componentName <> "id"
+        component = m.global.testCoverage[componentName]
         uncoveredLines = []
-        linesCount = component.count()
-        for i in linesCount
-            if component[i] <> true
-                uncoveredLines.pop(i)
-            end if
+        linesCount = component.length
+        for i = 0 to linesCount - 1
+          if component[i.toStr()] <> true
+            uncoveredLines.push(i)
+          end if
         end for
         coveredCount = linesCount - uncoveredLines.count()
         coveragePercent = (coveredCount / linesCount * 100).toStr()
-        ?"================================================================="
-        ?substitute("===   Component {} Test Suite:", component)
-        ?"==="
-        ?substitute("===   Total  = {} ; Covered  =  {} ; Uncovered   =  {} ; Coverage: {}%", linesCount, coveredCount, uncoveredLines.count(), coveragePercent)
-        ?"================================================================="
-    end for"""
+        ?"-----------------------------------------------------------------"
+        ?substitute("---   Component {0} Test Suite:", componentName)
+        ?"---"
+        ?substitute("---   Total  = {0} ; Covered  =  {1} ; Uncovered   =  {2} ; Coverage: {3}%", linesCount.toStr(), coveredCount.toStr(), uncoveredLines.count().toStr(), coveragePercent)
+        if uncoveredLines.count() > 0
+          uncoveredLinesString = "---   Uncovered Line(s): "
+          for each line in uncoveredLines
+            uncoveredLinesString += line.toStr() + ", "
+          end for
+          uncoveredLinesString = left(uncoveredLinesString, len(uncoveredLinesString) - 2)
+          ?uncoveredLinesString
+        end if
+        ?"-----------------------------------------------------------------"
+      end if
+      totalLinesCount += linesCount
+      totalUncoveredCount += uncoveredLines.count()
+    end for
+    totalCoveredCount = totalLinesCount - totalUncoveredCount
+    totalCoveragePercent = (totalCoveredCount / totalLinesCount * 100).toStr()
+    ?"================================================================="
+    ?"===   CODE COVERAGE SUMMARY REPORT"
+    ?"==="
+    ?substitute("===   Total  = {0} ; Covered  =  {1} ; Uncovered   =  {2} ; Coverage: {3}%", totalLinesCount.toStr(), (totalLinesCount - totalUncoveredCount).toStr(), totalUncoveredCount.toStr(), totalCoveragePercent)
+    ?"=================================================================" """
 
 def is_balanced(block):
     if block.count("(") != block.count(")"):
@@ -109,9 +179,11 @@ def transform_component(component):
         component_texts[i] = component_text
 
     coverage_init = COVERAGE_INIT.format(component_name, line_num)
-    insert_index = init_function.index("\n")
-    init_out = init_function[:insert_index] + coverage_init + init_function[insert_index:]
-    component_texts[component_main_index] = component_texts[component_main_index].replace(init_function, init_out)
+    mark_function = MARK_TEST_FUNC.format(component_name, line_num)
+    # insert_index = init_function.index("\n")
+    # init_out = mark_function + init_function[:insert_index] + coverage_init + init_function[insert_index:]
+    # component_texts[component_main_index] = component_texts[component_main_index].replace(init_function, init_out)
+    component_texts[component_main_index] = mark_function + component_texts[component_main_index]
 
     for component_file, component_text in zip(component_files, component_texts):
         print("Component file: " + component_file)
@@ -128,26 +200,30 @@ def transform_main(main_file):
 
 # read input & create directory
 import sys, os
-# project_dir = sys.argv[1]
-project_dir = "./lofi-protoman"
+project_dir = os.path.join(".", sys.argv[1])
+# project_dir = "./lofi-protoman"
 if not os.path.exists(project_dir):
     print("Project not found")
     sys.exit()
 
 coverage_dir = project_dir + "-coverage"
+# print(coverage_dir)
 if not os.path.exists(coverage_dir):
     os.mkdir(coverage_dir)
+    print("New directory created at {}".format(coverage_dir))
 
 # copy files from directory
 from distutils.dir_util import copy_tree
+print("Copying project...")
 copy_tree(project_dir, coverage_dir)
+print("Finished.")
 
 #get list of copied files
 # components_dir = os.path.join(coverage_dir, "components")
 component_files = []
 main_file = None
 for root, dirs, files in os.walk(coverage_dir):
-    if "tests" in root:
+    if "tests" in root or "tasks" in root:
         continue
     for name in files:
         if "source" in root and name == "main.brs":
@@ -158,9 +234,15 @@ component_files.sort()
 
 from itertools import groupby
 
-components = [(key, list(group)) for key, group in groupby(component_files, lambda f: f.split("/")[4].split(".")[0])]
+components = [(key.replace("-", ""), list(group)) for key, group in groupby(component_files, lambda f: f.split("/")[4].split(".")[0])]
 
 for c in components:
     # print(c)
+
     transform_component(c)
 transform_main(main_file)
+
+from subprocess import Popen
+os.environ["ROKU_DEV_TARGET"] = "192.168.2.89"
+os.environ["DEVPASSWORD"] = "aaaa"
+Popen(["make", "install"], cwd = coverage_dir)
